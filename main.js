02a4225b1c672af81aed01e4f3185b8ee3d3da0c
RaseckShow <ces_art11@hotmail.com>
2022-08-16 00:09:14 -0500

    Importacion de repositorio Challenge 01

---
+++ main.js
@@ -0,0 +1,29 @@
+let mensaje = document.getElementById("textAreaMensaje");
+let mostrarMensaje = document.getElementById("textAreaMostrarMensaje");
+function Encriptar()
+{
+    var texto = mensaje.value;
+    var codificado = texto.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");
+    OcultarElementos();
+    mostrarMensaje.value = codificado;
+}
+function Desencriptar()
+{
+    var texto = mensaje.value;
+    var codificado = texto.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");
+    OcultarElementos();
+    mostrarMensaje.value = codificado;
+}
+function CopiarTexto()
+{
+    var textoResultante = mostrarMensaje.value;
+    navigator.clipboard.writeText(textoResultante);
+}
+function OcultarElementos()
+{
+    document.getElementById("imagenMostrarMensaje").style.display = "none";
+    document.getElementById("textoNoEncontrado").style.display = "none";
+    document.getElementById("ingreseTexto").style.display = "none"; 
+    document.getElementById("botonCopiar").style.visibility = "visible";
+
+}
